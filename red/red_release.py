b9fffa2c33f724f90632710cfd0a667d1c32c0ac
David Frey <dfrey@sierrawireless.com>
2020-02-03 09:59:32 -0800

    add comments to red_release.py

--- red/red_release.py
+++ red/red_release.py
@@ -8,6 +8,12 @@ import shutil
 import pathlib
 
 def fetch_git_repo(url, ref):
+    """Clone the repository specified by url and checkout ref
+
+    The repository is cloned recursively, so submodules will also be retrieved. Note that the
+    repository is cloned into the current working directory and that the name of the directory will
+    be derived from the URL.
+    """
     repo = url.split("/")[-1]
     if repo.endswith('.git'):
         repo = repo[:-4]
@@ -16,18 +22,25 @@ def fetch_git_repo(url, ref):
     subprocess.run("git submodule update --recursive --init", cwd=repo, check=True, shell=True)
 
 
-def force_clean_octave():
-    for dir, subdirs, files in os.walk("brkedgepkg"):
+def force_clean_git_repo(repo_path):
+    """Recursively find all git repositories under repo_path and remove untracked files"""
+    for dir, subdirs, files in os.walk(repo_path):
         if '.git' in subdirs or '.git' in files:
             print("cleaning: {}".format(dir))
             subprocess.run("git clean -xfd", check=True, shell=True, cwd=dir)
 
+
 def build_octave_packages(json_bs):
+    """Build Octave and create Leaf packages"""
     fetch_git_repo("git@github.com:flowthings/brkedgepkg.git", json_bs["octave_git_ref"])
     fetch_git_repo("git@github.com:mangOH/mangOH.git", json_bs["mangoh_git_ref"])
 
     for module in json_bs["modules"]:
-        force_clean_octave()
+        # It seems that it is necessary to clean the brkedgepkg between each
+        # build for a different module. I suspect that the build system for
+        # jerryscript isn't smart enough to know that it needs to re-build
+        # certain artifacts when the toolchain is swapped out.
+        force_clean_git_repo("brkedgepkg")
         subprocess.run(
             "leaf setup -p swi-{}_{} _tmp_red_release".format(
                 module["casual_target"], module["master_package_version"]),
@@ -50,6 +63,7 @@ def build_octave_packages(json_bs):
 
 
 def build_master_packages(json_bs):
+    """Build the master packages for all of the modules listed in the build specification file"""
     for module in json_bs["modules"]:
         pathlib.Path("manifests/{}".format(module["casual_target"])).mkdir(parents=True, exist_ok=True)
         subprocess.run(
@@ -88,10 +102,12 @@ def build_master_packages(json_bs):
 
 
 def build_index():
+    """Build the Leaf index for all of the packages in the leaf directory"""
     subprocess.run("leaf build index -o mangOH-red.json *.leaf", check=True, shell=True, cwd="leaf")
 
 
 def red_build(build_spec):
+    """Clone, build and package for all the modules in the given build specification"""
     json_bs = None
     with open(build_spec) as bs:
         json_bs = json.load(bs)
